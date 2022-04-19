91ccc1783e2ee9f3277cbea0ec175dc35124ffe3
Ziyu Huang <aprilhuang825@gmail.com>
2022-04-19 04:23:41 +0000

    refactor getUserWithEmail getUserWithId and addUser functions

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -17,7 +17,7 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
+/*   let user;
   for (const userId in users) {
     user = users[userId];
     if (user.email.toLowerCase() === email.toLowerCase()) {
@@ -26,7 +26,11 @@ const getUserWithEmail = function(email) {
       user = null;
     }
   }
-  return Promise.resolve(user);
+  return Promise.resolve(user); */
+  return pool
+  .query(`SELECT * FROM users WHERE users.email = $1`, [email])
+  .then(res => res.rows[0] || null)
+  .catch(err => console.error('query error', err));
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,7 +40,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  /* return Promise.resolve(users[id]); */
+  return pool
+  .query(`SELECT * FROM users WHERE users.id = $1`, [id])
+  .then(res => res.rows[0] || null)
+  .catch(err => console.error('query error', err));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -47,10 +55,21 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
+/*   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
-  return Promise.resolve(user);
+  return Promise.resolve(user); */
+  const queryString = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `;
+  const values = [user.name, user.email, user.password];
+
+  return pool
+  .query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.error('query error', err));
 }
 exports.addUser = addUser;
 
